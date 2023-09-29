ad6d0de31a6dfb75ddea4ce8596a244bb61b98d6
Suphitchapipe <suphitchaprite@gmail.com>
2023-09-29 19:55:02 +0700

    Fourt - uploading code

--- Assignment02/catalog_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Product, Traveler
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Traveler)
